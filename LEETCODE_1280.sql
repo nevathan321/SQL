914f28268f7c414e12413d66b27d04bb35830c7f
nevathan321 <150630749+nevathan321@users.noreply.github.com>
2025-10-15 02:17:05 -0400

    Redid LEETCODE 1280

--- LEETCODE_1280.sql
+++ LEETCODE_1280.sql
@@ -1,16 +1,11 @@
 SELECT 
-    s.student_id,
-    s.student_name,
-    sub.subject_name,
-    COUNT(e.subject_name) AS attended_exams
-FROM 
-    Students s
-CROSS JOIN 
-    Subjects sub
-LEFT JOIN 
-    Examinations e 
-    ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
-GROUP BY 
-    s.student_id, s.student_name, sub.subject_name
-ORDER BY 
-    s.student_id, sub.subject_name;
+  s.student_id,
+  s.student_name,
+  sb.subject_name,
+  Count(e.subject_name) as attended_exams
+From Students as s 
+Cross join Subjects as sb 
+Left join Examinations as e
+on s.student_id = e.student_id and sb.subject_name = e.subject_name
+GROUP by s.student_id, s.student_name, sb.subject_name 
+Order by s.student_id, sb.subject_name 
