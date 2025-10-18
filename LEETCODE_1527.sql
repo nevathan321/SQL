74eee4470a353ab764d8a69a891081887311a16f
nevathan321 <150630749+nevathan321@users.noreply.github.com>
2025-10-18 17:44:40 -0400

    pandas version for leetcode 1527

--- LEETCODE_1527.sql
+++ LEETCODE_1527.sql
@@ -5,3 +5,13 @@ SELECT
 FROM Patients
 WHERE conditions LIKE 'DIAB1%'      
    OR conditions LIKE '% DIAB1%';   
+
+
+
+/* pandas */ 
+
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    result = patients[patients['conditions'].str.contains(r'(^|\s)DIAB1[0-9]*($|\s)' , regex=True na=False)]
+    return result
